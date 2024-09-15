14a93dbb6642b0fed3423048b5f027f97eae45d7
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 19:39:26 +0530

    Update halfofnumber.c

--- halfofnumber.c
+++ halfofnumber.c
@@ -1,10 +1,10 @@
-// #include<stdio.h> 
-// int main(){
-// float a,b ;
-//   printf("enter a number ;");
-//   scanf("%f",&a);
-//   b = a/2;
-//   printf("the half of tjhe number is : %f",b);
+#include<stdio.h> 
+int main(){
+float a,b ;
+  printf("enter a number ;");
+  scanf("%f",&a);
+  b = a/2;
+  printf("the half of tjhe number is : %f",b);
 
-// return 0;
-// }
+return 0;
+}
