c0e5446eecf9d2ea6eb722d13ab20e04f2373fed
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 19:38:55 +0530

    Update simpleintrest.c

--- simpleintrest.c
+++ simpleintrest.c
@@ -1,11 +1,11 @@
 
-// #include<stdio.h>
-// int main (){
-//  float p,r,t,si ;
-//   p= 588;
-//     r = 50;
-//   t = 60;
-//   si= p*r*t/100;
-//   printf("simple intrest :   %f",si);
-// return 0;
-// }
+#include<stdio.h>
+int main (){
+ float p,r,t,si ;
+  p= 588;
+    r = 50;
+  t = 60;
+  si= p*r*t/100;
+  printf("simple intrest :   %f",si);
+return 0;
+}
