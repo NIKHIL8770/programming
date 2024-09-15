d1fb1878a97bc7a70de549067be959f16cb4b36f
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 19:37:41 +0530

    Update volumeofsphere.c

--- volumeofsphere.c
+++ volumeofsphere.c
@@ -1,9 +1,9 @@
-// #include<stdio.h> 
-// int main(){
-//  float pi, r, A;
-//   pi = 3.14;
-//   r = 99.75;
-//   A = 4*pi*r*r*r/3;
-//   printf("volume of shere ;  %f",A);
-// return 0;
-// }
+#include<stdio.h> 
+int main(){
+ float pi, r, A;
+  pi = 3.14;
+  r = 99.75;
+  A = 4*pi*r*r*r/3;
+  printf("volume of shere ;  %f",A);
+return 0;
+}
