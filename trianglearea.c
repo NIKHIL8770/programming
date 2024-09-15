c56e851ccc53dc024e5415faa14554ac760b4162
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 19:38:18 +0530

    Update trianglearea.c

--- trianglearea.c
+++ trianglearea.c
@@ -1,9 +1,9 @@
-// #include<stdio.h> 
-// int main (){
-//   float a = 1.732 ;
-//   float b = 4 ;
-//   float c = 22 ;
-//   float area = a/b*c*c ;
-//   printf("area of equiletrial triangle %f",area);
-// return 0;
-// }
+#include<stdio.h> 
+int main (){
+  float a = 1.732 ;
+  float b = 4 ;
+  float c = 22 ;
+  float area = a/b*c*c ;
+  printf("area of equiletrial triangle %f",area);
+return 0;
+}
