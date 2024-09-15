acaad848fa0b35a754c52233d5ffedc2588b9104
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 19:37:57 +0530

    Update variable.c

--- variable.c
+++ variable.c
@@ -1,10 +1,10 @@
-//  #include<stdio.h>
-// int main (){
-//      int x; //declaration
-//        x = 77; //initilazation
-//      printf("%d\n",x+44);
-//   int b  = 23;
-//    printf("%d",b+44);
+ #include<stdio.h>
+int main (){
+     int x; //declaration
+       x = 77; //initilazation
+     printf("%d\n",x+44);
+  int b  = 23;
+   printf("%d",b+44);
 
-//  return 0;
-// }
+ return 0;
+}
